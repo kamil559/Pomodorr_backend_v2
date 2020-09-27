435d8993b23ea3a28c3abb943438f7f539c7d4cf
kamil559 <kamil.daniewski@gmail.com>
2020-09-27 22:33:52 +0200

    Fixed finish_pomodoro use case accordingly to the underlying domain logic.

--- pomodoro_system/pomodoros/pomodoros/application/use_cases/finish_pomodoro.py
+++ pomodoro_system/pomodoros/pomodoros/application/use_cases/finish_pomodoro.py
@@ -2,19 +2,26 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import datetime
 
+from foundation.application.repositories.user import UsersRepository
+from foundation.domain.entities.user import UserId
+from foundation.value_objects import DateFrameDefinition
+from pomodoros.application.queries.pomodoros import GetRecentPomodoros
 from pomodoros.application.repositories.pomodoros import PomodorosRepository
-from pomodoros.domain.value_objects import DateFrameId, FrameType
+from pomodoros.application.repositories.tasks import TasksRepository
+from pomodoros.domain.entities import Task
+from pomodoros.domain.value_objects import FrameType, PomodoroId
 
 
 @dataclass
 class FinishPomodoroInputDto:
-    id: DateFrameId
+    id: PomodoroId
     end_date: datetime
+    owner_id: UserId
 
 
 @dataclass
 class FinishPomodoroOutputDto:
-    id: DateFrameId
+    id: PomodoroId
     start_date: datetime
     end_date: datetime
     frame_type: FrameType
@@ -27,15 +34,30 @@ class FinishPomodoroOutputBoundary(ABC):
 
 
 class FinishPomodoro:
-    def __init__(self, output_boundary: FinishPomodoroOutputBoundary, date_frames_repository: PomodorosRepository):
+    def __init__(self, output_boundary: FinishPomodoroOutputBoundary,
+                 pomodoros_repository: PomodorosRepository, tasks_repository: TasksRepository,
+                 users_repository: UsersRepository, recent_pomodoros_query: GetRecentPomodoros) -> None:
         self.output_boundary = output_boundary
-        self.date_frames_repository = date_frames_repository
+        self.pomodoros_repository = pomodoros_repository
+        self.tasks_repository = tasks_repository
+        self.users_repository = users_repository
+        self.recent_pomodoros_query = recent_pomodoros_query
+
+    def _get_date_frame_definition(self, task: Task, user_id: UserId) -> DateFrameDefinition:
+        if task.date_frame_definition is not None:
+            return task.date_frame_definition
+
+        task_owner = self.users_repository.get(user_id=user_id)
+        return task_owner.date_frame_definition
 
     def execute(self, input_dto: FinishPomodoroInputDto) -> None:
-        pomodoro = self.date_frames_repository.get(pomodoro_id=input_dto.id)
-        pomodoro.finish(end_date=input_dto.end_date)
-        self.date_frames_repository.save(pomodoro)
+        pomodoro = self.pomodoros_repository.get(input_dto.id)
+        task = self.tasks_repository.get(pomodoro.task_id)
+        date_frame_definition = self._get_date_frame_definition(task, input_dto.owner_id)
+        recent_pomodoros = self.recent_pomodoros_query.query(task.id)
 
-        output_dto = FinishPomodoroOutputDto(id=pomodoro.id, start_date=pomodoro.start_date, end_date=pomodoro.end_date,
-                                             frame_type=pomodoro.frame_type)
-        self.output_boundary.present(output_dto=output_dto)
+        pomodoro.finish(date_frame_definition, task, recent_pomodoros, input_dto.end_date)
+        self.pomodoros_repository.save(pomodoro)
+
+        output_dto = FinishPomodoroOutputDto(pomodoro.id, pomodoro.start_date, pomodoro.end_date, pomodoro.frame_type)
+        self.output_boundary.present(output_dto)
