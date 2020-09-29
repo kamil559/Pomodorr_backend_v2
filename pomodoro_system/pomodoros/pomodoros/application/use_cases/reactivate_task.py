52a2396f3b5ad2d5bba7aa933672d06fe9a54e8c
kamil559 <kamil.daniewski@gmail.com>
2020-09-29 11:37:19 +0200

    Fixed reactivate_task use case based on the Task entity's method.

--- pomodoro_system/pomodoros/pomodoros/application/use_cases/reactivate_task.py
+++ pomodoro_system/pomodoros/pomodoros/application/use_cases/reactivate_task.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+from pomodoros.application.queries.tasks import GetTasksByPomodoroId
 from pomodoros.application.repositories.tasks import TasksRepository
 from pomodoros.domain.value_objects import TaskStatus, TaskId
 
@@ -23,13 +24,17 @@ class ReactivateTaskOutputBoundary(ABC):
 
 
 class ReactivateTask:
-    def __init__(self, output_boundary: ReactivateTaskOutputBoundary, tasks_repository: TasksRepository) -> None:
+    def __init__(self, output_boundary: ReactivateTaskOutputBoundary, tasks_repository: TasksRepository,
+                 get_tasks_by_pomodoro_id_query: GetTasksByPomodoroId) -> None:
         self.output_boundary = output_boundary
         self.tasks_repository = tasks_repository
+        self.get_tasks_by_pomodoro_id_query = get_tasks_by_pomodoro_id_query
 
     def execute(self, input_dto: ReactivateTaskInputDto) -> None:
         task = self.tasks_repository.get(task_id=input_dto.id)
-        task.reactivate()
+        task_project_tasks_collection = self.get_tasks_by_pomodoro_id_query.query(task.project_id)
+
+        task.reactivate(task_project_tasks_collection)
         self.tasks_repository.save(task)
 
         output_dto = ReactivateTaskOutputDto(id=task.id, status=task.status)
