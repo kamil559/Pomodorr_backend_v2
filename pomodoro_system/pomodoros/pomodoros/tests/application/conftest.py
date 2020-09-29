606ee6b4a8607d05d53f4cc5712d66c67f410aac
kamil559 <kamil.daniewski@gmail.com>
2020-09-29 22:06:21 +0200

    Added test for finish_pomodoro use case.

--- pomodoro_system/pomodoros/pomodoros/tests/application/conftest.py
+++ pomodoro_system/pomodoros/pomodoros/tests/application/conftest.py
@@ -3,10 +3,14 @@ from unittest.mock import Mock
 
 import pytest
 
+from foundation.application.repositories.user import UsersRepository
+from foundation.domain.entities.user import AbstractUser
+from foundation.domain.tests.factories import UserFactory
 from pomodoros.application.queries.pomodoros import GetRecentPomodoros
 from pomodoros.application.repositories.pomodoros import PomodorosRepository
 from pomodoros.application.repositories.tasks import TasksRepository
 from pomodoros.application.use_cases.begin_pomodoro import (BeginPomodoro, BeginPomodoroOutputBoundary)
+from pomodoros.application.use_cases.finish_pomodoro import FinishPomodoroOutputBoundary, FinishPomodoro
 from pomodoros.domain.entities import Task, Project
 from pomodoros.domain.entities.pomodoro import Pomodoro
 from pomodoros.tests.application.get_recent_pomodoros_query import GetRecentPomodorosStub
@@ -16,19 +20,25 @@ from pomodoros.tests.factories import PomodoroFactory, TaskFactory
 
 
 @pytest.fixture()
+def user() -> AbstractUser:
+    return UserFactory()
+
+
+@pytest.fixture()
 def begin_pomodoro_output_boundary() -> Mock:
     return Mock(spec_set=BeginPomodoroOutputBoundary)
 
 
 @pytest.fixture()
-def recent_pomodoros_list(task: Task) -> List[Pomodoro]:
-    pomodoros = [PomodoroFactory(task_id=task.id), PomodoroFactory(task_id=task.id), PomodoroFactory(task_id=task.id)]
-    return pomodoros
+def pomodoros_repository() -> PomodorosRepository:
+    return InMemoryPomodorosRepository()
 
 
 @pytest.fixture()
-def pomodoros_repository() -> PomodorosRepository:
-    return InMemoryPomodorosRepository()
+def populated_pomodoros_repository(started_pomodoro: Pomodoro) -> PomodorosRepository:
+    other_pomodoros = [PomodoroFactory(task_id=started_pomodoro.task_id),
+                       PomodoroFactory(task_id=started_pomodoro.task_id)]
+    return InMemoryPomodorosRepository(initial_data=[started_pomodoro] + other_pomodoros)
 
 
 @pytest.fixture()
@@ -38,8 +48,8 @@ def tasks_repository() -> TasksRepository:
 
 @pytest.fixture()
 def populated_tasks_repository(task: Task, project: Project) -> TasksRepository:
-    another_tasks = [TaskFactory(project_id=task.project_id), TaskFactory(project_id=task.project_id)]
-    return InMemoryTasksRepository(initial_data=[task] + another_tasks)
+    other_tasks = [TaskFactory(project_id=task.project_id), TaskFactory(project_id=task.project_id)]
+    return InMemoryTasksRepository(initial_data=[task] + other_tasks)
 
 
 @pytest.fixture()
@@ -48,13 +58,38 @@ def recent_pomodoros_query() -> GetRecentPomodoros:
 
 
 @pytest.fixture()
+def recent_pomodoros_list(task: Task) -> List[Pomodoro]:
+    pomodoros = [PomodoroFactory(task_id=task.id), PomodoroFactory(task_id=task.id), PomodoroFactory(task_id=task.id)]
+    return pomodoros
+
+
+@pytest.fixture()
 def populated_recent_pomodoros_query(recent_pomodoros_list: List[Pomodoro]):
     return GetRecentPomodorosStub(return_collection=recent_pomodoros_list)
 
 
 @pytest.fixture()
-def begin_pomodoro_use_case(begin_pomodoro_output_boundary: Mock, pomodoros_repository,
-                            populated_tasks_repository, populated_recent_pomodoros_query) -> BeginPomodoro:
+def begin_pomodoro_use_case(begin_pomodoro_output_boundary, pomodoros_repository, populated_tasks_repository,
+                            populated_recent_pomodoros_query) -> BeginPomodoro:
     return BeginPomodoro(output_boundary=begin_pomodoro_output_boundary, pomodoros_repository=pomodoros_repository,
                          tasks_repository=populated_tasks_repository,
                          recent_pomodoros_query=populated_recent_pomodoros_query)
+
+
+@pytest.fixture()
+def users_repository(user: AbstractUser) -> Mock:
+    return Mock(spec_set=UsersRepository, get=Mock(return_value=user))
+
+
+@pytest.fixture()
+def finish_pomodoro_output_boundary() -> Mock:
+    return Mock(spec_set=FinishPomodoroOutputBoundary)
+
+
+@pytest.fixture()
+def finish_pomodoro_use_case(finish_pomodoro_output_boundary, populated_pomodoros_repository,
+                             populated_tasks_repository, users_repository, populated_recent_pomodoros_query):
+    return FinishPomodoro(output_boundary=finish_pomodoro_output_boundary,
+                          pomodoros_repository=populated_pomodoros_repository,
+                          tasks_repository=populated_tasks_repository, users_repository=users_repository,
+                          recent_pomodoros_query=populated_recent_pomodoros_query)
---
+++ pomodoro_system/pomodoros/pomodoros/tests/application/use_cases/test_finish_pomodoro.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from pomodoros.application.use_cases.finish_pomodoro import FinishPomodoroInputDto, FinishPomodoroOutputDto
+
+
+def test_finish_pomodoro_use_case(user, started_pomodoro, finish_pomodoro_output_boundary, finish_pomodoro_use_case,
+                                  populated_pomodoros_repository):
+    now = datetime.now()
+    input_dto = FinishPomodoroInputDto(id=started_pomodoro.id, end_date=now, owner_id=user.id)
+
+    finish_pomodoro_use_case.execute(input_dto=input_dto)
+    expected_dto = FinishPomodoroOutputDto(id=started_pomodoro.id, start_date=started_pomodoro.start_date, end_date=now,
+                                           frame_type=started_pomodoro.frame_type)
+
+    mutated_pomodoro = populated_pomodoros_repository.get(started_pomodoro.id)
+
+    assert mutated_pomodoro.end_date == now
+    finish_pomodoro_output_boundary.present.assert_called_once_with(expected_dto)
