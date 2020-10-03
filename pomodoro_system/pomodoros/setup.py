76f478e184f84c02004b1058c24a92407f52ac0d
kamil559 <kamil.daniewski@gmail.com>
2020-10-03 19:10:06 +0200

    From now on there are no per package dependencies since the packages will remain in the project and global dependencies are suitable (introduced Poetry to manage the dependencies).

--- pomodoro_system/pomodoros/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="pomodoros",
-    version="0.0.0",
-    packages=find_packages(),
-    install_requires=["injector", "foundation"],
-    extras_require={"dev": ["pytest"]},
-)
