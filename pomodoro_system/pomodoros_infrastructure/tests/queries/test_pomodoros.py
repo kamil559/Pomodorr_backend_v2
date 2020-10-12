488b31463c00b3f9395fe186dc85f67d82f8ce0e
kamil559 <kamil.daniewski@gmail.com>
2020-10-12 21:23:23 +0200

    Added tests for SQLGetRecentPomodoros.

---
+++ pomodoro_system/pomodoros_infrastructure/tests/queries/test_pomodoros.py
@@ -0,0 +1,28 @@
+from pony.orm import db_session
+
+from pomodoros_infrastructure.queries.pomodoros import SQLGetRecentPomodoros
+
+
+def test_get_recent_pomodoros_returns_pomodoro_from_today(orm_task, orm_pomodoro_for_today, orm_pomodoro_for_yesterday):
+    query_object = SQLGetRecentPomodoros()
+    with db_session:
+        result = query_object.query(orm_task.id)
+
+    assert len(result) == 1
+    assert orm_pomodoro_for_yesterday.id != result[0].id
+    assert orm_pomodoro_for_today.id == result[0].id
+
+
+def test_get_recent_pomodoros_returns_task_related_pomodoros_only(orm_task, orm_pomodoro_for_today,
+                                                                  orm_random_pomodoro_for_today,
+                                                                  orm_pomodoro_for_yesterday,
+                                                                  orm_random_pomodoro_for_yesterday):
+    query_object = SQLGetRecentPomodoros()
+    with db_session:
+        result = query_object.query(orm_task.id)
+
+    assert len(result) == 1
+    assert orm_pomodoro_for_yesterday.id != result[0].id
+    assert orm_random_pomodoro_for_yesterday.id != result[0].id
+    assert orm_random_pomodoro_for_today.id != result[0].id
+    assert orm_pomodoro_for_today.id == result[0].id
