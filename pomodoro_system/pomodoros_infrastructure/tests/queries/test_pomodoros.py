521bd318affaabc1ee75a04b6abfe46fc758b8aa
kamil559 <kamil.daniewski@gmail.com>
2020-10-13 10:04:39 +0200

    Test functions have been put into classes in order to separate the tests for a given feature (in case the test files grow in size in the future).

--- pomodoro_system/pomodoros_infrastructure/tests/queries/test_pomodoros.py
+++ pomodoro_system/pomodoros_infrastructure/tests/queries/test_pomodoros.py
@@ -1,31 +1,50 @@
+import uuid
+
 import pytest
 from pony.orm import db_session
 
 from pomodoros_infrastructure.queries.pomodoros import SQLGetRecentPomodoros
 
-pytestmark = pytest.mark.usefixtures("setup_teardown_tables")
 
+@pytest.mark.usefixtures("setup_teardown_tables")
+class TestGetRecentPomodorosQuery:
 
-def test_get_recent_pomodoros_returns_pomodoro_from_today(orm_task, orm_pomodoro_for_today, orm_pomodoro_for_yesterday):
-    query_object = SQLGetRecentPomodoros()
-    with db_session:
-        result = query_object.query(orm_task.id)
+    def test_query_returns_pomodoro_from_today(self, orm_task, orm_pomodoro_for_today, orm_pomodoro_for_yesterday):
+        query_object = SQLGetRecentPomodoros()
+        with db_session:
+            result = query_object.query(orm_task.id)
 
-    assert len(result) == 1
-    assert orm_pomodoro_for_yesterday.id != result[0].id
-    assert orm_pomodoro_for_today.id == result[0].id
+        assert len(result) == 1
+        assert orm_pomodoro_for_yesterday.id != result[0].id
+        assert orm_pomodoro_for_today.id == result[0].id
 
+    def test_query_returns_task_related_pomodoros_only(self, orm_task, orm_pomodoro_for_today,
+                                                       orm_random_pomodoro_for_today,
+                                                       orm_pomodoro_for_yesterday,
+                                                       orm_random_pomodoro_for_yesterday):
+        query_object = SQLGetRecentPomodoros()
+        with db_session:
+            result = query_object.query(orm_task.id)
 
-def test_get_recent_pomodoros_returns_task_related_pomodoros_only(orm_task, orm_pomodoro_for_today,
-                                                                  orm_random_pomodoro_for_today,
-                                                                  orm_pomodoro_for_yesterday,
-                                                                  orm_random_pomodoro_for_yesterday):
-    query_object = SQLGetRecentPomodoros()
-    with db_session:
-        result = query_object.query(orm_task.id)
+        assert len(result) == 1
+        assert orm_pomodoro_for_yesterday.id != result[0].id
+        assert orm_random_pomodoro_for_yesterday.id != result[0].id
+        assert orm_random_pomodoro_for_today.id != result[0].id
+        assert orm_pomodoro_for_today.id == result[0].id
 
-    assert len(result) == 1
-    assert orm_pomodoro_for_yesterday.id != result[0].id
-    assert orm_random_pomodoro_for_yesterday.id != result[0].id
-    assert orm_random_pomodoro_for_today.id != result[0].id
-    assert orm_pomodoro_for_today.id == result[0].id
+    def test_query_returns_none_if_task_has_no_recent_pomodoros(self, orm_task):
+        query_object = SQLGetRecentPomodoros()
+
+        with db_session:
+            result = query_object.query(orm_task.id)
+
+        assert result is None
+
+    def test_query_returns_none_if_non_existing_task_id_was_passed(self):
+        query_object = SQLGetRecentPomodoros()
+        random_uuid = uuid.uuid4()
+
+        with db_session:
+            result = query_object.query(random_uuid)
+
+        assert result is None
--- pomodoro_system/pomodoros_infrastructure/tests/queries/test_tasks.py
+++ pomodoro_system/pomodoros_infrastructure/tests/queries/test_tasks.py
@@ -1,17 +1,36 @@
+import uuid
+
 import pytest
 from pony.orm import db_session
 
 from pomodoros_infrastructure.queries.tasks import SQLGetTasksByProjectId
 
-pytestmark = pytest.mark.usefixtures("setup_teardown_tables")
 
+@pytest.mark.usefixtures("setup_teardown_tables")
+class TestGetTaskByProjectIdQuery:
+    def test_query_returns_tasks_by_project_id(self, orm_project, orm_task, orm_random_task):
+        query_object = SQLGetTasksByProjectId()
 
-def test_query_returns_tasks_by_project_id(orm_project, orm_task, orm_random_task):
-    query_object = SQLGetTasksByProjectId()
+        with db_session:
+            result = query_object.query(orm_project.id)
 
-    with db_session:
-        result = query_object.query(orm_project.id)
+        assert len(result) == 1
+        assert orm_random_task.id != result[0].id
+        assert orm_task.id == result[0].id
 
-    assert len(result) == 1
-    assert orm_random_task.id != result[0].id
-    assert orm_task.id == result[0].id
+    def test_query_returns_none_if_project_has_no_tasks(self, orm_project):
+        query_object = SQLGetTasksByProjectId()
+
+        with db_session:
+            result = query_object.query(orm_project.id)
+
+        assert result is None
+
+    def test_query_returns_none_if_non_existing_project_id_was_passed(self):
+        query_object = SQLGetTasksByProjectId()
+        random_uuid = uuid.uuid4()
+
+        with db_session:
+            result = query_object.query(random_uuid)
+
+        assert result is None
